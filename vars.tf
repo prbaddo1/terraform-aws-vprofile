348b5334b0cfebe54fb46bad6f174a6699c7f713
Prince Addo <prince.addo1@yahoo.com>
2022-07-22 23:49:40 -0500

    vpc module

--- vars.tf
+++ vars.tf
@@ -1,95 +1,95 @@
-variable AWS_REGION {
-    default = "us-east-2"
+variable "AWS_REGION" {
+  default = "us-east-2"
 }
 
-variable AIMS {
-    type = map
-    default =  {
-        us-east-2 = "ami-0960ab670c8bb45f3"
-        us-east-1 = "ami-08d4ac5b634553e16"
-    }
+variable "AIMS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-0960ab670c8bb45f3"
+    us-east-1 = "ami-08d4ac5b634553e16"
+  }
 }
 
-variable PRIV_KEY_PATH {
-    default = "vprofilekey"
+variable "PRIV_KEY_PATH" {
+  default = "vprofilekey"
 }
 
-variable PUB_KEY_PATH {
-    default = "vprofilekey.pub"
+variable "PUB_KEY_PATH" {
+  default = "vprofilekey.pub"
 }
 
-variable USERNAME {
-    default = "ubuntu"
+variable "USERNAME" {
+  default = "ubuntu"
 }
 
-variable MYIP {
-    default = "192.168.33.1/32"
+variable "MYIP" {
+  default = "192.168.33.1/32"
 }
 
-variable rmquser {
-    default = "rabbit"
+variable "rmquser" {
+  default = "rabbit"
 }
 
-variable rmqpass {
-    default = "Gr33n@pple123456"
+variable "rmqpass" {
+  default = "Gr33n@pple123456"
 }
 
-variable dbuser {
-    default = "admin"
+variable "dbuser" {
+  default = "admin"
 }
 
-variable dbpass {
-    default = "admin123"
+variable "dbpass" {
+  default = "admin123"
 }
 
-variable dbname {
-    default = "accounts"
+variable "dbname" {
+  default = "accounts"
 }
 
-variable instance_count {
-    default = "1"
+variable "instance_count" {
+  default = "1"
 }
 
-variable VPC_NAME {
-    default = "vprofile-VPC"
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
 }
 
-variable Zone1 {
-    default = "us-east-2a"
+variable "Zone1" {
+  default = "us-east-2a"
 }
 
-variable Zone2 {
-    default = "us-east-2b"
+variable "Zone2" {
+  default = "us-east-2b"
 }
 
-variable Zone3 {
-    default = "us-east-2c"
+variable "Zone3" {
+  default = "us-east-2c"
 }
 
-variable VpcCIDR {
-    default = "172.21.0.0/16"
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
 }
 
-variable PubSub1CIDR {
-    default = "172.21.1.0/24"
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
 }
 
-variable PubSub2CIDR {
-    default = "172.21.2.0/24"
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
 }
 
-variable PubSub3CIDR {
-    default = "172.21.3.0/24"
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
 }
 
-variable PrivSub1CIDR {
-    default = "172.21.4.0/24"
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
 }
 
-variable PrivSub2CIDR {
-    default = "172.21.5.0/24"
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
 }
 
-variable PrivSub3CIDR {
-    default = "172.21.6.0/24"
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
 }
